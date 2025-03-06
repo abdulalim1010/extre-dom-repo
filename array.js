d95f9f079e94faba299a78c97b99b263c07708ed
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-06 17:31:37 +0600

    evening time

---
+++ array.js
@@ -0,0 +1,8 @@
+const friends = ["hena", "ali", "raju","hensa"];
+for (let i = 0; i < friends.length; i++){
+  const element = friends[i];
+  
+}
+for (const element of friends) {
+console.log(element)
+}
---
+++ function.js
@@ -0,0 +1,5 @@
+function sum(a, b,c) {
+  console.log(a,b,c,a+b+c);
+}
+sum(5, 56,28)
+console.log(sum.length)
